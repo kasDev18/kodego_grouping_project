9bfce6956775887c20b08781acff1f124e0597e5
kasDev18 <kascatindoy@yahoo.com>
2022-06-19 02:14:09 +0800

    create login page and title icon

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const cardTitle = document.querySelector('.card-title');
-cardTitle.addEventListener('click',function(){
-    alert('hey')
+// login page
+const loginForm = document.querySelector('.login_form')
+const welcomeLogin = document.getElementById('login_username_label');
+const usernameLogin = document.getElementById('login_username');
+const loginInputs = document.querySelectorAll('#exampleInput');
+const loginInputsCheckBox = document.getElementById('exampleInputCheckBox');
+const copyrightDate = document.querySelector('.copyright_date')
+
+loginForm.addEventListener('input', (e) =>{
+    if (usernameLogin.value === '' || usernameLogin.value === undefined)
+    {
+        welcomeLogin.innerText = ``;
+    }
+    else
+    {
+        welcomeLogin.innerText = `Welcome ${usernameLogin.value}!`
+    }
 })
-cardTitle.innerHTML.toUpperCase();
+
+let copyright = new Date().getFullYear();
+copyrightDate.innerText = copyright;
+
